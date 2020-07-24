5341a0617e66fcdb47894a005defd6f469a0d0cb
Snazyl <iliapetrovs@hotmail.co.uk>
2020-07-24 13:36:38 +0100

    final touch ups

---
+++ Assets/Scripts/DragHandler.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public static GameObject itemDragged;
+    Transform startParent;
+    public GameObject panelDefault;
+    public List<GameObject> panelAway = new List<GameObject>();
+    Vector3 startPosition;
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        itemDragged = gameObject;
+        startPosition = transform.position;
+        startParent = transform.parent;
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+        itemDragged.GetComponent<LayoutElement>().ignoreLayout = true;
+        itemDragged.transform.SetParent(itemDragged.transform.parent.parent);
+    }
+
+
+    public void OnDrag(PointerEventData eventData)
+    {
+
+        transform.position = Input.mousePosition;
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        itemDragged = null;
+
+        transform.position = transform.parent.position;
+
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        itemDragged.GetComponent<LayoutElement>().ignoreLayout = false;
+    }
+
+    public void OnClick()
+    {
+        foreach (var item in panelAway)
+        {
+            if (item.activeSelf)
+            {
+                item.SetActive(false);
+            }
+        }
+        panelDefault.SetActive(!panelDefault.activeSelf);
+    }
+
+}
---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class Slot : MonoBehaviour, IDropHandler
+{
+
+    public GameObject item
+    {
+        get
+        {
+            if (transform.childCount > 0)
+            {
+                return transform.GetChild(0).gameObject;
+            }
+            return null;
+        }
+    }
+    #region IDropHandler implementation
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Transform parent = DragHandler.itemDragged.transform.parent;
+        if (!item)
+        {
+
+            DragHandler.itemDragged.transform.SetParent(transform);
+
+        }
+        else
+        {
+            DragHandler.itemDragged.transform.SetParent(transform);
+            transform.GetChild(0).SetParent(parent);
+        }
+    }
+    #endregion
+
+}
